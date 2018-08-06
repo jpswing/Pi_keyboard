dad698793463b60b15a923bc34e10a9bf1d700f8
Swing <sgnxydx3211@163.com>
2018-08-06 14:02:28 +0800

    add distortion FX control

--- test.c
+++ test.c
@@ -10,10 +10,12 @@ struct fx_data_t {
 	float val;
 };
 int instrument = 0, octave = 0;
-fluid_settings_t* settings;
-fluid_synth_t* synth;
+fluid_settings_t* settings = NULL;
+fluid_synth_t* synth = NULL;
 fluid_audio_driver_t* adriver = NULL;
 int CMajor[7] = {60, 62, 64, 65, 67, 69, 71};
+
+int distortion = 0;
 
 void showInst(fluid_sfont_t *sf, int preNum) {
 	fluid_preset_t* ps = sf->get_preset(sf, 0, preNum);
@@ -26,13 +28,22 @@ int fx_func(void *data, int len, int nin, float **in, int nout, float **out) {
 	if (fluid_synth_process(fx_data->synth, len, nin, in, nout, out) != 0) {
 		return -1;
 	}
-	for (int i = 0; i < nout; ++i) {
-		float *out_i = out[i];
-		for (int k = 0; k < len; ++k) {
-			int x = out_i[k];
-			if (abs(x) < 1e-8) continue;
-			int y = x * x / abs(x);
-			out_i[k] = x / abs(x) * (1 - exp(y));
+	if (distortion) {
+		for (int i = 0; i < nout; ++i) {
+			float *out_i = out[i];
+			for (int k = 0; k < len; ++k) {
+				float x = out_i[k] * 2;
+				
+				if (x > 0) out_i[k] = 1 - exp(-x);
+				else out_i[k] = -1 + exp(x);
+
+				/*
+				if (abs(x) < 1e-8) continue;
+				float y = x * x / abs(x);
+				out_i[k] = x / abs(x) * (1 - exp(-y));
+				*/
+				// printf("%.2f, %.2f ", x, out_i[k]);
+			}
 		}
 	}
 	return 0;
@@ -82,7 +93,7 @@ void noteControl() {
 
 int main(int argc, char** argv)
 {
-	fluid_sfont_t* sfont;
+	fluid_sfont_t* sfont = NULL;
 	struct fx_data_t fx_data;
 	int sfont_id;
 	int ret;
@@ -107,9 +118,9 @@ int main(int argc, char** argv)
 		fprintf(stderr, "cannot change audio driver\n");
 		goto cleanUp;
 	}
-	ret = fluid_settings_setnum(settings, "synth.gain", 1.0);
+	ret = fluid_settings_setnum(settings, "synth.gain", 1.5);
 	if (ret == FLUID_FAILED) {
-		fprintf(stderr, "cannot change gain\n");
+		fprintf(stderr, "cannot set gain\n");
 		goto cleanUp;
 	}
 	
@@ -119,10 +130,8 @@ int main(int argc, char** argv)
 	fx_data.synth = synth;
 
 	/* Create the audio driver. The synthesizer starts playing as soon
-		as the driver is created. No fx. */
-	adriver = new_fluid_audio_driver(settings, synth);
-	/* Use fx. */
-	// adriver = new_fluid_audio_driver2(settings, fx_func, (void *) &fx_data);
+	 * as the driver is created. */
+	adriver = new_fluid_audio_driver2(settings, fx_func, (void *) &fx_data);
 	
 	/* Load a SoundFont and reset presets (so that new instruments
 	 * get used from the SoundFont) */
@@ -134,14 +143,15 @@ int main(int argc, char** argv)
 	sfont = fluid_synth_get_sfont_by_id(synth, sfont_id);
 	showInst(sfont, instrument);
 	
-	/* Initialize the random number generator */
-	// srand(getpid());
-	
 	printf("ready\n");
 
 	for (;;) {
 		noteControl();
 		octaveControl();
+
+		if (!digitalRead(25)) distortion = 1;
+		else distortion = 0;
+		
 		/*
 		if (!digitalRead(1)) {
 			ret = fluid_synth_program_change(synth, 0, ++instrument);
